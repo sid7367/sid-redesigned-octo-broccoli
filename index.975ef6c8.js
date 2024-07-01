6c0163777aaee01897c436fcbc6456f02cce9161
Sidhaarth Mohandas <147407494+sid7367@users.noreply.github.com>
2024-07-01 10:50:11 +0000

    Updates

--- index.975ef6c8.js
+++ index.975ef6c8.js
@@ -28622,9 +28622,9 @@ var _propTypesDefault = parcelHelpers.interopDefault(_propTypes);
  *
  * Need an image? Check out https://unsplash.com to download a photo you
  * freely use on your site.
- */ var _boyUsingVRARJpg = require("../images/boy using VR-AR.jpg");
-var _boyUsingVRARJpgDefault = parcelHelpers.interopDefault(_boyUsingVRARJpg);
-const imageAltText = "Boy using VR headset and AR glasses learning about technology.";
+ */ var _whiteHomepageJpg = require("../images/white homepage.jpg");
+var _whiteHomepageJpgDefault = parcelHelpers.interopDefault(_whiteHomepageJpg);
+const imageAltText = "white tiles design";
 const Home = ({ name , title  })=>{
     return /*#__PURE__*/ (0, _jsxDevRuntime.jsxDEV)("section", {
         id: "home",
@@ -28632,7 +28632,7 @@ const Home = ({ name , title  })=>{
         children: [
             /*#__PURE__*/ (0, _jsxDevRuntime.jsxDEV)("img", {
                 className: "background",
-                src: (0, _boyUsingVRARJpgDefault.default),
+                src: (0, _whiteHomepageJpgDefault.default),
                 alt: ""
             }, void 0, false, {
                 fileName: "src/Components/Home.jsx",
@@ -28715,11 +28715,11 @@ $RefreshReg$(_c, "Home");
   window.$RefreshReg$ = prevRefreshReg;
   window.$RefreshSig$ = prevRefreshSig;
 }
-},{"react/jsx-dev-runtime":"iTorj","react":"21dqq","../images/down-arrow.svg":"b5nCi","prop-types":"7wKI2","../images/boy using VR-AR.jpg":"2tmmJ","@parcel/transformer-js/src/esmodule-helpers.js":"gkKU3","@parcel/transformer-react-refresh-wrap/lib/helpers/helpers.js":"km3Ru"}],"b5nCi":[function(require,module,exports) {
+},{"react/jsx-dev-runtime":"iTorj","react":"21dqq","../images/down-arrow.svg":"b5nCi","prop-types":"7wKI2","@parcel/transformer-js/src/esmodule-helpers.js":"gkKU3","@parcel/transformer-react-refresh-wrap/lib/helpers/helpers.js":"km3Ru","../images/white homepage.jpg":"kSqC6"}],"b5nCi":[function(require,module,exports) {
 module.exports = require("./helpers/bundle-url").getBundleURL("bLxZJ") + "down-arrow.1037091f.svg" + "?" + Date.now();
 
-},{"./helpers/bundle-url":"lgJ39"}],"2tmmJ":[function(require,module,exports) {
-module.exports = require("./helpers/bundle-url").getBundleURL("bLxZJ") + "boy using VR-AR.83d118bd.jpg" + "?" + Date.now();
+},{"./helpers/bundle-url":"lgJ39"}],"kSqC6":[function(require,module,exports) {
+module.exports = require("./helpers/bundle-url").getBundleURL("bLxZJ") + "white homepage.9f263e47.jpg" + "?" + Date.now();
 
 },{"./helpers/bundle-url":"lgJ39"}],"lCaEt":[function(require,module,exports) {
 var $parcel$ReactRefreshHelpers$ed0d = require("@parcel/transformer-react-refresh-wrap/lib/helpers/helpers.js");
