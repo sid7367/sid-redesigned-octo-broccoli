5c227e82b47a9cd342c0f0fae4d517c7ad2f244e
Sidhaarth Mohandas <147407494+sid7367@users.noreply.github.com>
2024-07-01 05:19:39 +0000

    updated the home page and added a new image

--- src/Components/Home.jsx
+++ src/Components/Home.jsx
@@ -20,9 +20,9 @@ import PropTypes from "prop-types";
  * Need an image? Check out https://unsplash.com to download a photo you
  * freely use on your site.
  */
-import image from "../images/boy using VR-AR.jpg";
+import image from "../images/goldstein.jpg";
 
-const imageAltText = "Boy using VR headset and AR glasses learning about technology.";
+const imageAltText = "Integrated Circuit designing on a computer";
 
 const Home = ({ name, title }) => {
   return (
