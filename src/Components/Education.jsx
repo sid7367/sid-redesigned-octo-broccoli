1c83c49aecb023364725e4b86926ed1f8e87d88f
Sidhaarth Mohandas <147407494+sid7367@users.noreply.github.com>
2024-07-01 12:08:47 +0000

    code changes in education.jsx

--- src/Components/Education.jsx
+++ src/Components/Education.jsx
@@ -1,36 +1,11 @@
 import React from "react";
-import PropTypes from "prop-types";
 
-const Education = ({ education }) => {
+const Education = () => {
   return (
-    <section id="education" className="container">
-      <h1>Education</h1>
-      <ul>
-        {education.map((item) => (
-          <li key={item.degree}>
-            <h2>{item.degree}</h2>
-            <h3>{item.institution}</h3>
-            <p>
-              {item.graduated}
-              <br />
-              {item.description}
-            </p>
-          </li>
-        ))}
-      </ul>
+    <section className="light" id="education">
+      <h2>Education</h2>
     </section>
   );
-};
-
-Education.propTypes = {
-  education: PropTypes.arrayOf(
-    PropTypes.shape({
-      degree: PropTypes.string.isRequired,
-      institution: PropTypes.string.isRequired,
-      graduated: PropTypes.string.isRequired,
-      description: PropTypes.string.isRequired,
-    })
-  ).isRequired,
 };
 
 export default Education;
