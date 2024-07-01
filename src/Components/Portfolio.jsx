16a81195eb5ae781a5e0bf1a075ffe8ecc761943
Sidhaarth Mohandas <147407494+sid7367@users.noreply.github.com>
2024-07-01 11:54:11 +0000

    removed animation to test the difference

--- src/Components/Portfolio.jsx
+++ src/Components/Portfolio.jsx
@@ -68,7 +68,7 @@ const Portfolio = () => {
               height: "90%",
               width: "100%",
               objectFit: "cover",
-              animation: "1s ease-out 0s 1 slideInLeft ",
+              // animation: "1s ease-out 0s 1 slideInLeft ",
             }}
             alt={imageAltText}
           />
